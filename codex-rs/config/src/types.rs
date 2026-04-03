fcoury/codex

feat(tui): add configurable Vim keymap and startup default
Expose Vim bindings through the TUI keymap config and add a startup
toggle so Vim mode can be enabled by default.

Wire runtime keymap handling for Vim actions, add
`tui.vim_mode_default` to config parsing/runtime config, apply the
default in ChatWidget startup paths, and update docs/default
keymap/schema accordingly.

codex-rs/core/src/config/types.rs
@@ -605,6 +605,11 @@ pub struct Tui {
     #[serde(default = "default_true")]
     pub show_tooltips: bool,
 
+    /// Start the composer in Vim mode (`Normal`) by default.
+    /// Defaults to `false`.
+    #[serde(default)]
+    pub vim_mode_default: bool,
+
     /// Controls whether the TUI uses the terminal's alternate screen buffer.
     ///
     /// - `auto` (default): Disable alternate screen in Zellij, enable elsewhere.
@@ -648,6 +653,8 @@ pub use super::tui_keymap::TuiKeymapPreset;
 pub use super::tui_keymap::TuiListKeymap;
 pub use super::tui_keymap::TuiOnboardingKeymap;
 pub use super::tui_keymap::TuiPagerKeymap;
+pub use super::tui_keymap::TuiVimNormalKeymap;
+pub use super::tui_keymap::TuiVimOperatorKeymap;
 
 /// Settings for notices we display to users via the tui and app-server clients
 /// (primarily the Codex IDE extension). NOTE: these are different from

blog2i2j/openai.._..codex

codex-rs/config/src/types.rs
@@ -37,6 +37,8 @@ pub use crate::tui_keymap::TuiKeymapPreset;
 pub use crate::tui_keymap::TuiListKeymap;
 pub use crate::tui_keymap::TuiOnboardingKeymap;
 pub use crate::tui_keymap::TuiPagerKeymap;
+pub use crate::tui_keymap::TuiVimNormalKeymap;
+pub use crate::tui_keymap::TuiVimOperatorKeymap;
 
 pub const DEFAULT_OTEL_ENVIRONMENT: &str = "dev";
 pub const DEFAULT_MEMORIES_MAX_ROLLOUTS_PER_STARTUP: usize = 16;
@@ -486,6 +488,11 @@ pub struct Tui {
     /// Defaults to `true`.
     #[serde(default = "default_true")]
     pub show_tooltips: bool,
+
+    /// Start the composer in Vim mode (`Normal`) by default.
+    /// Defaults to `false`.
+    #[serde(default)]
+    pub vim_mode_default: bool,
 
     /// Controls whether the TUI uses the terminal's alternate screen buffer.
     ///