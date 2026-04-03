openai/codex

feat(core): add keymap config schema and types
Introduce keymap configuration types and schema support in core without
wiring runtime key handling yet. This keeps behavior unchanged while
adding the configuration surface needed by later commits.

codex-rs/core/src/config/types.rs
@@ -623,11 +623,31 @@ pub struct Tui {
     /// `current-dir`.
     #[serde(default)]
     pub status_line: Option<Vec<String>>,
+
+    /// Keybinding overrides for the TUI.
+    ///
+    /// This supports rebinding selected actions globally and by context.
+    /// Context bindings take precedence over `global` bindings.
+    #[serde(default)]
+    pub keymap: TuiKeymap,
 }
 
 const fn default_true() -> bool {
     true
 }
+
+pub use super::tui_keymap::KeybindingSpec;
+pub use super::tui_keymap::KeybindingsSpec;
+pub use super::tui_keymap::TuiApprovalKeymap;
+pub use super::tui_keymap::TuiChatKeymap;
+pub use super::tui_keymap::TuiComposerKeymap;
+pub use super::tui_keymap::TuiEditorKeymap;
+pub use super::tui_keymap::TuiGlobalKeymap;
+pub use super::tui_keymap::TuiKeymap;
+pub use super::tui_keymap::TuiKeymapPreset;
+pub use super::tui_keymap::TuiListKeymap;
+pub use super::tui_keymap::TuiOnboardingKeymap;
+pub use super::tui_keymap::TuiPagerKeymap;
 
 /// Settings for notices we display to users via the tui and app-server clients
 /// (primarily the Codex IDE extension). NOTE: these are different from

blog2i2j/openai.._..codex

codex-rs/config/src/types.rs
@@ -25,6 +25,19 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use serde::Serialize;
 
+pub use crate::tui_keymap::KeybindingSpec;
+pub use crate::tui_keymap::KeybindingsSpec;
+pub use crate::tui_keymap::TuiApprovalKeymap;
+pub use crate::tui_keymap::TuiChatKeymap;
+pub use crate::tui_keymap::TuiComposerKeymap;
+pub use crate::tui_keymap::TuiEditorKeymap;
+pub use crate::tui_keymap::TuiGlobalKeymap;
+pub use crate::tui_keymap::TuiKeymap;
+pub use crate::tui_keymap::TuiKeymapPreset;
+pub use crate::tui_keymap::TuiListKeymap;
+pub use crate::tui_keymap::TuiOnboardingKeymap;
+pub use crate::tui_keymap::TuiPagerKeymap;
+
 pub const DEFAULT_OTEL_ENVIRONMENT: &str = "dev";
 pub const DEFAULT_MEMORIES_MAX_ROLLOUTS_PER_STARTUP: usize = 16;
 pub const DEFAULT_MEMORIES_MAX_ROLLOUT_AGE_DAYS: i64 = 30;
@@ -506,6 +519,13 @@ pub struct Tui {
     /// Use `/theme` in the TUI or see `$CODEX_HOME/themes` for custom themes.
     #[serde(default)]
     pub theme: Option<String>,
+
+    /// Keybinding overrides for the TUI.
+    ///
+    /// This supports rebinding selected actions globally and by context.
+    /// Context bindings take precedence over `global` bindings.
+    #[serde(default)]
+    pub keymap: TuiKeymap,
 
     /// Startup tooltip availability NUX state persisted by the TUI.
     #[serde(default)]